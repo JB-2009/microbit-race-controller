790a692303f92c2c5bc04ac92f700b83f38086d0
JB-2009 <j.bellaart@hotmail.com>
2025-08-26 09:46:39 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,25 @@
+input.onButtonPressed(Button.A, function () {
+    radio.sendNumber(5)
+})
+input.onGesture(Gesture.LogoUp, function () {
+    radio.sendNumber(3)
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    radio.sendNumber(2)
+})
+input.onButtonPressed(Button.AB, function () {
+    radio.sendNumber(7)
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendNumber(6)
+})
+input.onGesture(Gesture.TiltRight, function () {
+    radio.sendNumber(4)
+})
+input.onGesture(Gesture.LogoDown, function () {
+    radio.sendNumber(1)
+})
+basic.forever(function () {
+    radio.setGroup(208)
+    radio.setTransmitPower(7)
+})
